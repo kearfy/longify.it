324bb6c255233da4f2cc218e8e125cadcbdf56c8
Micha de Vries <micha@devrie.sh>
2022-10-26 21:50:24 +0200

    Update longify.ts

--- longify.ts
+++ longify.ts
@@ -1,4 +1,6 @@
 export default {
-    "i can't do that": "Due to my inability of everything non minimalistic, I must now announce that I, unfortunately, am not able to comply with the given request",
-    "i cannot do that": "link:i can't do that"
+    "i cannot": "I must hereby announce that I, the author of this here paper, do not find the current circumstances described to be advantageous enough to allow myself to have the potential to act, within my own capacity, upon the course of events thus previously described. Therefore, the author, which is me, has come to the conclusion that I, unfortunately, am unable to comply with the given request",
+    "i can't do that": "link:i cannot",
+    "i cannot do that": "link:i cannot",
+    "i can't": "link:i cannot"
 }
