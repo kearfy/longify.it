33f13161b366cf45aebdf2161e4b51a6a5bc6123
Micha de Vries <micha@devrie.sh>
2022-10-26 21:22:01 +0200

    Longify it

---
+++ longify.ts
@@ -0,0 +1,4 @@
+export default {
+    "i can't do that": "Due to my inability of everything non minimalistic, I must now announce that I, unfortunately, am not able to comply with the given request",
+    "i cannot do that": "link:i can't do that"
+}
